611c800be68d4ef395a456c7dae6240ebe1c7849
Shaun Thompson <shaunthompson@fullsail.edu>
2013-03-07 20:44:45 -0500

    Setup html layout for divs

--- thompson_shaun_project1/js/json.js
+++ thompson_shaun_project1/js/json.js
@@ -35,5 +35,133 @@ var json = {
 		"name": ["Food Name:", "Crackers"],
 		"calories": ["Calorie Amount:", "240"],
 		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry5": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry6": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry7": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry8": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry9": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry10": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry11": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry12": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry13": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry14": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry15": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry16": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry17": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry18": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry19": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry20": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type", "Other"],
+		"group": ["Food Group", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
 	}
 }
