e7a6c60eae244f49a9bfbcc9a510a27fcab1e2bd
Shaun Thompson <shaunthompson@fullsail.edu>
2013-03-20 21:25:18 -0400

    Refactored JS files to use JQM

--- thompson_shaun_project3/GoldApp/js/nmain.js
+++ thompson_shaun_project3/GoldApp/js/nmain.js
@@ -1,67 +1,33 @@
+//Shaun Thompson
+//MiU 1303 - Project 3 [modified JS file]
+//03/21/2013
+
+var parseHealthForm = function(data){
+	console.log(data);
+};
+
 $(document).ready(function(){
 
-	var rbform = $('#recordbootyform');
+	var calform = $('#calform'),
+		errorslink = $('#errorslink')
+	;
 	
-	rbform.validate();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
-
-
-
-/*
- $('#home').on('pageinit', function(){
-	//code needed for home page goes here
-});	
-		
-$('#addItem').on('pageinit', function(){
-
-		var myForm = $('#formId');
-		    myForm.validate({
-			invalidHandler: function(form, validator) {
-			},
-			submitHandler: function() {
-		var data = myForm.serializeArray();
-			storeData(data);
+	calform.validate({
+		invalidHandler: function(form, validator){
+			errorslink.click();
+			var html = '';
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]');
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += '<li>'+ fieldName +'</li>';
+			};
+			$("#formerrors ul").html(html);
+		},
+		submitHandler: function(){
+			var data = calform.seralizeArray();
+			parseHealthForm(data);
 		}
 	});
-	
-	//any other code needed for addItem page goes here
-	
+
 });
-
-//The functions below can go inside or outside the pageinit function for the page in which it is needed.
-
-var autofillData = function (){
-	 
-};
-
-var getData = function(){
-
-};
-
-var storeData = function(data){
-	
-}; 
-
-var	deleteItem = function (){
-			
-};
-					
-var clearLocal = function(){
-
-};
-*/
