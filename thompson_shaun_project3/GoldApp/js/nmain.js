0d21c195ea21158e1d685b7027ce047351b44373
Shaun Thompson <shaunthompson@fullsail.edu>
2013-03-21 19:31:54 -0400

    Completed tracker form and customization. Need to finalize reworking of JS

--- thompson_shaun_project3/GoldApp/js/nmain.js
+++ thompson_shaun_project3/GoldApp/js/nmain.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			errorslink.click();
 			var html = '';
 			for(var key in validator.submitted){
-				var label = $('label[for^="'+ key +'"]');
+				var label = $('label[for^="'+ key +'"]').not('[generated]');
 				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
 				var fieldName = legend.length ? legend.text() : label.text();
 				html += '<li>'+ fieldName +'</li>';
