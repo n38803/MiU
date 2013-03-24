d60946e2d94d2140f89050ddc0fc4be3a063caae
Shaun Thompson <shaunthompson@fullsail.edu>
2013-03-24 19:14:02 -0400

    Created new files for project 4 - changed repository and adjusted files.

---
+++ thompson_shaun_project4/BronzeApp/js/json.js
@@ -0,0 +1,167 @@
+//Shaun Thompson
+//MiU 1303 - Project 3
+//03/21/13
+//JSON Object
+
+var json = {
+	"entry1": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type:", "Breakfast"],
+		"group": ["Food Group:", "Dairy"],
+		"name": ["Food Name:", "Milk"],
+		"calories": ["Calorie Amount:", "180"],
+		"notes": ["Additional Notes:", "12 ounce Glass"]
+	},
+	"entry2": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type:", "Lunch"],
+		"group": ["Food Group:", "Vegetable"],
+		"name": ["Food Name:", "Orange"],
+		"calories": ["Calorie Amount:", "320"],
+		"notes": ["Additional Notes:", "3 Oranges"]
+	},	
+	"entry3": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type:", "Dinner"],
+		"group": ["Food Group:", "Meat"],
+		"name": ["Food Name:", "Steak"],
+		"calories": ["Calorie Amount:", "580"],
+		"notes": ["Additional Notes:", "12 ounce steak"]
+	},
+	"entry4": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type:", "Other"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Crackers"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Half a pack of crackers as a snack"]
+	},
+		"entry5": {
+		"date": ["Date:", "2013-02-28"],
+		"type": ["Meal Type:", "Snack"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Snack Bar"],
+		"calories": ["Calorie Amount:", "440"],
+		"notes": ["Additional Notes:", "N/A"]
+	},
+		"entry6": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Breakfast"],
+		"group": ["Food Group:", "Fruit"],
+		"name": ["Food Name:", "Grape Fruit"],
+		"calories": ["Calorie Amount:", "300"],
+		"notes": ["Additional Notes:", "Wanted More!"]
+	},
+		"entry7": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Lunch"],
+		"group": ["Food Group:", "Vegetable"],
+		"name": ["Food Name:", "Salad"],
+		"calories": ["Calorie Amount:", "250"],
+		"notes": ["Additional Notes:", "Also had crutons and shredded cheese. Yum!"]
+	},
+		"entry8": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Lunch"],
+		"group": ["Food Group:", "Dairy"],
+		"name": ["Food Name:", "Shredded Cheese"],
+		"calories": ["Calorie Amount:", "300"],
+		"notes": ["Additional Notes:", "Salad!"]
+	},
+		"entry9": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Lunch"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Crutons"],
+		"calories": ["Calorie Amount:", "100"],
+		"notes": ["Additional Notes:", "Salad!"]
+	},
+		"entry10": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Snack"],
+		"group": ["Food Group:", "Fruit"],
+		"name": ["Food Name:", "Orange"],
+		"calories": ["Calorie Amount:", "200"],
+		"notes": ["Additional Notes:", "Shoulda Had juice!"]
+	},
+		"entry11": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Other"],
+		"group": ["Food Group:", "Fruit"],
+		"name": ["Food Name:", "Apple Juice"],
+		"calories": ["Calorie Amount:", "180"],
+		"notes": ["Additional Notes:", "Got my juice."]
+	},
+		"entry12": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Dinner"],
+		"group": ["Food Group:", "Meat"],
+		"name": ["Food Name:", "Fried Chicken"],
+		"calories": ["Calorie Amount:", "580"],
+		"notes": ["Additional Notes:", "2 pieces"]
+	},
+		"entry13": {
+		"date": ["Date:", "2013-03-01"],
+		"type": ["Meal Type:", "Dinner"],
+		"group": ["Food Group:", "Vegetable"],
+		"name": ["Food Name:", "Cream Corn"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "10 ounces"]
+	},
+		"entry14": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Breakfast"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Honey Bunches of Oats"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "Full bowl"]
+	},
+		"entry15": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Breakfast"],
+		"group": ["Food Group:", "Dairy"],
+		"name": ["Food Name:", "Milk"],
+		"calories": ["Calorie Amount:", "150"],
+		"notes": ["Additional Notes:", "8 ounces"]
+	},
+		"entry16": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Breakfast"],
+		"group": ["Food Group:", "Fruit"],
+		"name": ["Food Name:", "Orange Juice"],
+		"calories": ["Calorie Amount:", "240"],
+		"notes": ["Additional Notes:", "8 ounces"]
+	},
+		"entry17": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Lunch"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Graham Crakcers"],
+		"calories": ["Calorie Amount:", "300"],
+		"notes": ["Additional Notes:", "Half a box, oh no!!"]
+	},
+		"entry18": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Dinner"],
+		"group": ["Food Group:", "Meat"],
+		"name": ["Food Name:", "Lamb"],
+		"calories": ["Calorie Amount:", "360"],
+		"notes": ["Additional Notes:", "Tasty!"]
+	},
+		"entry19": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Snack"],
+		"group": ["Food Group:", "Dairy"],
+		"name": ["Food Name:", "Ice Cream"],
+		"calories": ["Calorie Amount:", "400"],
+		"notes": ["Additional Notes:", "Mmmm!"]
+	},
+		"entry20": {
+		"date": ["Date:", "2013-03-02"],
+		"type": ["Meal Type:", "Other"],
+		"group": ["Food Group:", "Grain"],
+		"name": ["Food Name:", "Jimmies"],
+		"calories": ["Calorie Amount:", "140"],
+		"notes": ["Additional Notes:", "Right on top!"]
+	}
+}
